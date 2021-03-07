084e56793c61dd51228a05bd91a11aa3788647b1
monaant <79448034+monaant@users.noreply.github.com>
2021-03-07 21:04:18 +0100

    Add files via upload

---
+++ 01_types_variables/01 into Types Variables favorite number.rb
@@ -0,0 +1,7 @@
+puts "Your name:"
+name = gets.chomp
+puts "Your middle:"
+middle = gets.chomp
+puts "Your last:"
+last = gets.chomp
+puts "Your full name is: #{name} #{middle} #{last}"
---
+++ 01_types_variables/01 into Types Variables full name.rb
@@ -0,0 +1,3 @@
+puts "Your favorite number:"
+number = gets.chomp.to_i
+puts "Maybe it's your favorite number: #{number+1}"
